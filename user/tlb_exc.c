bbc2a10d0e292eb2a9ac9ae0bde7ee5627a1049d
Max Meidinger <max_meidinger@stud.uni-bamberg.de>
2024-07-16 15:35:39 +0200

    daily dump

--- user/tlb_exc.c
+++ user/tlb_exc.c
@@ -2,7 +2,7 @@
 #include "user/user.h"
 
 int main(int argc, char *argv[]) {
-  __asm__("li x1, 0x88000000\n\t \
-            lw x2, 0(x1)\n\t");
+  __asm__("li x1, 0x87fff000\n\t \
+            lw x2, 0(x1)\n\t"); //Weird output here when context jumps back with other reg contents -> not state restore!
   exit(0);
 }
