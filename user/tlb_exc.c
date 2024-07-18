62b4710985d40868ccb3edf00b49d5b0abcb25fa
Max Meidinger <max_meidinger@stud.uni-bamberg.de>
2024-07-18 17:03:31 +0200

    I overwrote the stack pointer

--- user/tlb_exc.c
+++ user/tlb_exc.c
@@ -2,11 +2,11 @@
 #include "user/user.h"
 
 int main(int argc, char *argv[]) {
-  __asm__("li x1, 0x84fff000\n\t \
-          li x2, 42\n\t \
-          sw x2, 0(x1)\n\t \
-          lw x3, 0(x1)\n\t"); //Weird output here when context jumps back with other reg contents -> not state restore!
-  register int foo asm ("x3");
+  __asm__("li s2, 0x84fff000\n\t \
+          li s3, 42\n\t \
+          sw s3, 0(s2)\n\t \
+          lw s4, 0(s2)\n\t"); //Weird output here when context jumps back with other reg contents -> not state restore!
+  register int *foo asm ("s4");
   printf("%d\n", foo);
   exit(0);
 }
